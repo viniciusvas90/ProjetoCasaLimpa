c94b167a365f327edc4b47ba756a22581f0c263f
vinicius.vas.ti <vinicius.vas.ti@gmail.com>
2017-11-22 21:26:50 -0300

    Criada a classe UtilVas

--- CasaLimpa/src/main/java/com/vas/casalimpa/CasalimpaApplication.java
+++ CasaLimpa/src/main/java/com/vas/casalimpa/CasalimpaApplication.java
@@ -1,12 +1,23 @@
 package com.vas.casalimpa;
 
+import com.vas.casalimpa.data.repository.IDiaristaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CasalimpaApplication {
+public class CasalimpaApplication implements CommandLineRunner {
+    
+    @Autowired
+    IDiaristaRepository diaristaRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(CasalimpaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CasalimpaApplication.class, args);
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        //diaristaRepository.count();
+    }
 }
