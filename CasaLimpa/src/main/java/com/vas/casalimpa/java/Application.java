d7d6dc39cf522c35fa97d4f6cf03cc47d1b3ffe7
Vinicius A dos Santos <vinicius.santos@ma9.com.br>
2017-12-05 15:23:22 -0300

    Importações organizadas

--- CasaLimpa/src/main/java/com/vas/casalimpa/java/Application.java
+++ CasaLimpa/src/main/java/com/vas/casalimpa/java/Application.java
@@ -1,7 +1,5 @@
 package com.vas.casalimpa.java;
 
-import com.vas.casalimpa.java.data.repository.IDiaristaRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
