2fab9ca4560be24de1627957fcd5a82304bff090
Vinicius A dos Santos <vinicius.santos@ma9.com.br>
2017-12-05 14:25:51 -0300

    Controle de acesso

---
+++ CasaLimpa/src/main/java/com/vas/casalimpa/java/controllers/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.vas.casalimpa.java.controllers;
+
+import com.vas.casalimpa.java.data.model.Usuario;
+import com.vas.casalimpa.java.data.repository.IUsuarioRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import static java.util.Collections.emptyList;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private IUsuarioRepository usuarioRepository;
+
+    public UserDetailsServiceImpl(IUsuarioRepository applicationUserRepository) {
+        this.usuarioRepository = applicationUserRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario user = usuarioRepository.FindUsuarioByUserName(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return new User(user.getUserName(), user.getSenha(), emptyList());
+    }
+}
